3cdf22243386b886884958c78986ecec4055c8c3
bubbleboy14 <mario.balibrera@gmail.com>
2021-12-16 02:16:55 -0800

    tests: added one; only 1st one worx :(

--- test/sweepsteak.js
+++ test/sweepsteak.js
@@ -12,15 +12,28 @@ const brax = [
 contract("SweepSteaks", async accounts => {
 	it("should accept bracket submissions", async () => {
 		let sweste = await SweepSteaks.deployed();
+		let phase = await sweste.phase();
+		console.log("phase:", phase);
 		for (var i = 1; i < 7; i++)
 			sweste.submitBracket(brax[i], { from: accounts[i] });
 	});
+	it("should shift to active phase", async () => {
+		let phase = await sweste.phase();
+		console.log("phase:", phase);
+		sweste.setActive({ from: accounts[0] });
+		let phase2 = await sweste.phase();
+		console.log("phase:", phase2);
+	});
 	it("should accept results submission", async () => {
 		let sweste = await SweepSteaks.deployed();
+		let phase = await sweste.phase();
+		console.log("phase:", phase);
 		sweste.submitResults([1, 3, 3], { from: accounts[0] });
 	});
 	it("should find winning brackets", async () => {
 		let sweste = await SweepSteaks.deployed();
+		let phase = await sweste.phase();
+		console.log("phase:", phase);
 		let winnerCount = await sweste.findWinningBrackets({ from: accounts[0] });
 		if (winnerCount == 2)
 			console.log("great! it counted right.");
