9d1e5e146425ab76deff76f2683e6f3577da7496
bubbleboy14 <mario.balibrera@gmail.com>
2021-12-16 02:50:52 -0800

    asserts

--- test/sweepsteak.js
+++ test/sweepsteak.js
@@ -14,27 +14,72 @@ contract("SweepSteaks", async accounts => {
 		let sweste = await SweepSteaks.deployed();
 		let phase = await sweste.phase();
 		console.log("phase:", phase);
-		for (var i = 1; i < 7; i++)
+		assert.equal(
+			phase.toNumber(),
+			0,
+			"phase should be Gather (0)"
+		);
+		for (var i = 1; i < 6; i++)
 			await sweste.submitBracket(brax[i], { from: accounts[i] });
+		let numBrax = await sweste.numBrackets();
+		var num = numBrax.toNumber();
+		console.log("brax:", num);
+		assert.equal(
+			num,
+			i,
+			"brax counted wrong!"
+		);
 	});
 	it("should shift to active phase", async () => {
 		let sweste = await SweepSteaks.deployed();
 		let phase = await sweste.phase();
-		console.log("phase:", phase);
+		var pnum = phase.toNumber();
+		console.log("phase:", pnum);
+		assert.equal(
+			pnum,
+			0,
+			"phase should be Gather (0)"
+		);
 		await sweste.setActive({ from: accounts[0] });
 		let phase2 = await sweste.phase();
-		console.log("phase:", phase2);
+		pnum = phase2.toNumber();
+		console.log("phase:", pnum);
+		assert.equal(
+			pnum,
+			1,
+			"phase should be Active (1)"
+		);
 	});
 	it("should accept results submission", async () => {
 		let sweste = await SweepSteaks.deployed();
 		let phase = await sweste.phase();
-		console.log("phase:", phase);
-		sweste.submitResults([1, 3, 3], { from: accounts[0] });
+		var pnum = phase.toNumber();
+		console.log("phase:", pnum);
+		assert.equal(
+			pnum,
+			1,
+			"phase should be Active (1)"
+		);
+		await sweste.submitResults([1, 3, 3], { from: accounts[0] });
+		let phase2 = await sweste.phase();
+		pnum = phase2.toNumber();
+		console.log("phase:", pnum);
+		assert.equal(
+			pnum,
+			2,
+			"phase should be Ended (2)"
+		);
 	});
 	it("should find winning brackets", async () => {
 		let sweste = await SweepSteaks.deployed();
 		let phase = await sweste.phase();
-		console.log("phase:", phase);
+		var pnum = phase.toNumber();
+		console.log("phase:", pnum);
+		assert.equal(
+			pnum,
+			2,
+			"phase should be Ended (2)"
+		);
 		let winnerCount = await sweste.findWinningBrackets({ from: accounts[0] });
 		if (winnerCount == 2)
 			console.log("great! it counted right.");
