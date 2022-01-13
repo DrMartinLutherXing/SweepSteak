208b2279ad04f438e27d1995c69fd5196fbfc90b
bubbleboy14 <mario.balibrera@gmail.com>
2022-01-12 22:34:29 -0800

    initial (ctinit-generated) project

---
+++ webui/js/core/config.js
@@ -0,0 +1,26 @@
+core.config = {
+    "log": {
+        "exclude": [], 
+        "include": []
+    }, 
+    "header": {
+        "logo": "Your Logo", 
+        "right": []
+    }, 
+    "keys": {}, 
+    "modals": {
+        "transition": "slide", 
+        "escape": true
+    }, 
+    "borderbox": true, 
+    "CC": {
+        "pod": null, 
+        "membership": null, 
+        "agent": null, 
+        "gateway": "https://care-coin.net/comp/api.js"
+    }, 
+    "mobile": {
+        "scale": false
+    }, 
+    "css": []
+};
