3594fc4561c282ebc67ff9fb9d46c46acbe0a88a
bubbleboy14 <mario.balibrera@gmail.com>
2022-12-06 01:24:46 -0800

    config: brakker{teams[],contract,abi} block (TODO abi/contract)

--- webui/js/core/config.js
+++ webui/js/core/config.js
@@ -1,6 +1,10 @@
 core.config = {
-    "teams": ["a", "b", "c", "d", "e", "f", "g", "h",
-        "i", "j", "k", "l", "m", "n", "o", "p"],
+    "brakker": {
+        "teams": ["a", "b", "c", "d", "e", "f", "g", "h",
+            "i", "j", "k", "l", "m", "n", "o", "p"],
+        "contract": "CONTRACT_ADDRESS",
+        "abi": null // path eg "abi.json"
+    },
     "log": {
         "exclude": [], 
         "include": []
