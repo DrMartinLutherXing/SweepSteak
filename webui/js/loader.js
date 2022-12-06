742e298109fd9822d30ef9760d4f4fb29381873e
bubbleboy14 <mario.balibrera@gmail.com>
2022-12-06 01:25:41 -0800

    loader: use ethers; brakker object with _{}, brak(), build(), load(), init()

--- webui/js/loader.js
+++ webui/js/loader.js
@@ -1,43 +1,60 @@
 CT.require("CT.all");
 CT.require("core");
+CT.scriptImport("https://cdn.ethers.io/lib/ethers-5.2.umd.min.js");
 
-var teams = core.config.teams;
+let cfg = core.config.brakker;
 
-var brak = function(teams, outres) {
-	var tlen = teams.length, t2 = tlen / 2,
-		n, rcell, b1, b2;
-	if (tlen == 1) // pre-final (side final)
-		n = CT.dom.div(teams[0], "cell");
-	else {
-		rcell = CT.dom.div("?", "result cell");
-		b1 = brak(teams.slice(0, t2), rcell);
-		b2 = brak(teams.slice(t2), rcell);
-		n = CT.dom.div([rcell, b1, b2], "cell");
-		outres || n.classList.add("outer");
-		outres || b2.classList.add("rightbrak");
-		b1.onclick = function(e) {
-			rcell.innerText = b1.innerText;
-			e.stopPropagation();
+let brakker = {
+	_: {},
+	brak: function(teams, outres) {
+		let tlen = teams.length, t2 = tlen / 2,
+			n, rcell, b1, b2, brak = brakker.brak;
+		if (tlen == 1) // pre-final (side final)
+			n = CT.dom.div(teams[0], "cell");
+		else {
+			rcell = CT.dom.div("?", "result cell");
+			b1 = brak(teams.slice(0, t2), rcell);
+			b2 = brak(teams.slice(t2), rcell);
+			n = CT.dom.div([rcell, b1, b2], "cell");
+			outres || n.classList.add("outer");
+			outres || b2.classList.add("rightbrak");
+			b1.onclick = function(e) {
+				rcell.innerText = b1.innerText;
+				e.stopPropagation();
+			};
+			b2.onclick = function(e) {
+				rcell.innerText = b2.innerText;
+				e.stopPropagation();
+			};
+			rcell.onclick = function(e) {
+				if (outres)
+					outres.innerText = rcell.innerText;
+				else
+					alert(rcell.innerText + " wins!");
+				e.stopPropagation();
+			};
+		}
+		n.value = function() {
+			return (rcell || n).innerText;
 		};
-		b2.onclick = function(e) {
-			rcell.innerText = b2.innerText;
-			e.stopPropagation();
-		};
-		rcell.onclick = function(e) {
-			if (outres)
-				outres.innerText = rcell.innerText;
-			else
-				alert(rcell.innerText + " wins!");
-			e.stopPropagation();
-		};
+		return n;
+	},
+	build: function() {
+		CT.dom.setMain(brakker.brak(cfg.teams));
+	},
+	load: function(abi) {
+		let _ = brakker._;
+		_.contract = new ethers.Contract(cfg.contract, abi, _.provider);
+		brakker.build();
+	},
+	init: function() {
+		let _ = brakker._;
+		_.provider = new ethers.providers.Web3Provider(window.ethereum);
+		cfg.abi ? fetch(cfg.abi).then(resp => resp.json()).then(brakker.load) : brakker.build();
 	}
-	n.value = function() {
-		return (rcell || n).innerText;
-	};
-	return n;
 };
 
 CT.onload(function() {
 	CT.initCore();
-	CT.dom.setMain(brak(teams));
+	brakker.init();
 });
